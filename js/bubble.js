0411e0bfabf683265ac679d92b86a0d72555b9d0
willc-cyber <weilian97@protonmail.com>
2024-11-21 16:10:48 +1100

    Updated js

--- js/bubble.js
+++ js/bubble.js
@@ -1,3 +1,5 @@
-document.getElementById('close-bubble').addEventListener('click', function() {
-    document.getElementById('floating-bubble').style.display = 'none';
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('close-bubble').addEventListener('click', function() {
+        document.getElementById('floating-bubble').style.display = 'none';
+    });
 });
